1f6ff6a56abb97d954c004e4c1254b90cdd8e726
IbrohimovDiyorbek <diyorbekibrohimov31@gmail.com>
2025-01-10 12:24:44 +0500

    it is a new restart

--- main.py
+++ main.py
@@ -1,19 +1,31 @@
 import asyncio
+import logging
 
 from aiogram import Bot, Dispatcher
-from config import TOKEN
-from handlers.start import router as start_router
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from handlers.start import router
+
+TOKEN = "7620217998:AAEytAOGAw7-KM6AU_LZ7LHQ6lfLhE7K9O8"
+
+bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+dp = Dispatcher()
 
 
 async def main():
-    bot = Bot(token=TOKEN)
-    dp = Dispatcher()
 
-    dp.include_router(start_router)
+    dp.include_routers(router)
 
-    print("Bot ishga tushdi...")
     await dp.start_polling(bot)
 
 
-if __name__ == "__main__":
-    asyncio.run(main())
+if __name__ == '__main__':
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(filename)s %(asctime)s [line: %(lineno)d] %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S"
+    )
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print("Good bye!")
